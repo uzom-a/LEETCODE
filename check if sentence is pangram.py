93fd4a2e5cd62dfd00520c82af3caffeb53a1f5f
Uzoma Stephanie-Portia Okey-Anyanwu <166033308+uzom-a@users.noreply.github.com>
2025-05-06 19:14:47 -0400

    Create check if sentence is pangram.py

---
+++ check if sentence is pangram.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        sentence = set(sentence)
+        
+        if len(sentence) == 26:
+            return True
+        else:
+            return False
