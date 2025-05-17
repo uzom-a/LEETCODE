6bf953bf525ac196165a8e5f7995fdd549c30976
Uzoma Stephanie-Portia Okey-Anyanwu <166033308+uzom-a@users.noreply.github.com>
2025-05-16 21:23:56 -0400

    Update Largest unique number.py

--- Largest unique number.py
+++ Largest unique number.py
@@ -18,3 +18,27 @@ class Solution:
                 ans = max(ans, key)
                 
         return ans if ans else -1
+
+"""
+SEE A CLEANER IMPLEMENTATION BELOW
+
+
+from collections import Counter
+
+class Solution:
+    def largestUniqueNumber(self, nums: List[int]) -> int:
+        """
+        #FIRST FUND OUT THE UNIQUE NUMBERS
+        unique_nums = set(nums)
+        this is a bit repititive because we can just use the hash map to 
+        """
+        ans = -1 # so that if there is no max the answer would be -1 as the question says
+        
+        count_nums = Counter(nums) #basically create the dictionary which would only have unique keys
+        
+        for key, value in count_nums.items():
+            if value == 1:
+                ans = max(ans, key)
+                
+        return ans 
+"""
