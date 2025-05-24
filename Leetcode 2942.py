cc62fe1a5db4605ae760dbc3c107d2d1a2b11ae8
Uzoma Stephanie-Portia Okey-Anyanwu <166033308+uzom-a@users.noreply.github.com>
2025-05-23 22:25:56 -0400

    Create Leetcode 2942.py

---
+++ Leetcode 2942.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        output = []
+        for i, word in enumerate(words):
+            if x in word:
+                output.append(i)
+
+        return output
