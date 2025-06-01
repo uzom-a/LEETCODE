15492c5ee055ec784bda317e2bb23c61fcfb8ecd
Uzoma Stephanie-Portia Okey-Anyanwu <166033308+uzom-a@users.noreply.github.com>
2025-05-31 21:36:06 -0400

    Create Isomorphic Strings.py

---
+++ Isomorphic Strings.py
@@ -0,0 +1,13 @@
+# Time Complexity : O(n)
+# Space Complexity : O(n)
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        map1 = []
+        map2 = []
+        for idx in s:
+            map1.append(s.index(idx)) #record the index by first occurence
+        for idx in t:
+            map2.append(t.index(idx)) #record the index by first occurence
+        if map1 == map2:
+            return True
+        return False
