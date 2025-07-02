ad6098db41117be6b79e266a92cafc313ccbfb24
Uzoma Stephanie-Portia Okey-Anyanwu <166033308+uzom-a@users.noreply.github.com>
2025-07-02 15:28:03 -0400

    Time: 39 ms (61.34%), Space: 17.9 MB (6.44%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.py
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.py
@@ -10,12 +10,19 @@ class Solution:
         while i < len(word):
             count = 0
             while i + 1 < len(word) and word[i] == word[i + 1]:
-                count += 1
+                count += 1 #so I am only keeping track of extras
                 i += 1
 
             if count >= 1:
                 length += (count)
-            print(count)
+
             i += 1
 
         return length
+
+        #Time Complexity is O(n)  #I only go through the string once
+        #Space Complexity is O(1)
+        """
+        next time, only keep track of extras 
+        it makes more sense to the problem of alice's keyboard anyways
+        """
