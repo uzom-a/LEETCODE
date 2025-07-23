256211d9f337ec9c9916ab82457e44ea0abf3eb1
Uzoma Stephanie-Portia Okey-Anyanwu <166033308+uzom-a@users.noreply.github.com>
2025-07-23 16:24:10 -0400

    Time: 11 ms (70.3%), Space: 18.3 MB (19.9%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -0,0 +1,27 @@
+from collections import deque
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        n = len(senate)
+        radiant = deque()
+        dire = deque()
+
+        for i ,s in enumerate(senate):
+            if s == "R":
+                radiant.append(i) #we are keeping track of the position
+            else:
+                dire.append(i)
+
+        while radiant and dire: # so that once one is empty we stop the loop
+            r = radiant.popleft()
+            d = dire.popleft()
+
+            if r < d: #the one that comes first kills the other
+                radiant.append(r + n) #The winner goes back in line with their index shifted by n to simulate the circular queue.
+            else:
+                dire.append(d + n)
+
+
+        return "Radiant" if radiant else "Dire"
+
+        #Time Complexity= 0(n)
+        #Space Complexity = O(n)
