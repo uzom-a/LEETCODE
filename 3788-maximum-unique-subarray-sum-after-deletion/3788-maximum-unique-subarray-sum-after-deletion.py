79d66fb0256b078869fbf719955b9c5fd7d9b88c
Uzoma Stephanie-Portia Okey-Anyanwu <166033308+uzom-a@users.noreply.github.com>
2025-07-24 22:10:09 -0400

    Time: 0 ms (100%), Space: 18 MB (11.24%) - LeetHub

---
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxSum(self, nums: List[int]) -> int:
+        maxx = max(nums)
+        i = 0
+        if maxx < 0:
+            return maxx
+        else:
+            nums = list(set(nums)) #removing duplicates to make the array unique
+            nums = [num if num > 0 else 0 for num in nums] #keeping only positive numbers
+        # print(nums)
+        return sum(nums)
+
+        #TIME COMPLEXITY : O(n)
+        # Space Complexity:O(n)
