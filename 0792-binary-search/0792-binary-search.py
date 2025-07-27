0b8eedd88ca4e1a4f5898588f68bc8887a833267
Uzoma Stephanie-Portia Okey-Anyanwu <166033308+uzom-a@users.noreply.github.com>
2025-07-26 22:22:33 -0400

    Time: 0 ms (100%), Space: 18.7 MB (44.81%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -2,10 +2,8 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         if nums == []:
             return -1
-
-        if len(nums) == 1 and nums[0]== target:
+        if len(nums) == 1 and nums[0] == target:
             return 0
-
         left = 0
         right = len(nums) - 1
 
@@ -13,12 +11,11 @@ class Solution:
             mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
-            if nums[mid] > target:
+            elif nums[mid] > target:
                 right = mid - 1
-            if nums[mid] < target:
+            else:
                 left = mid + 1
- 
-        return -1
+                
 
-    # Time Complexity: O(log n)
-    # Space Complexity: O(1)
+
+        return -1
