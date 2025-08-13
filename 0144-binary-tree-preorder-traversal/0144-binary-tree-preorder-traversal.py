f61ffad61392cd2d8d00f1c46df71caf8a90c541
Uzoma Stephanie-Portia Okey-Anyanwu <166033308+uzom-a@users.noreply.github.com>
2025-08-13 01:16:56 -0500

    Time: 0 ms (100%), Space: 17.7 MB (53.79%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        def dfs(node, result):
+            if not node:
+                return []
+
+            result.append(node.val)
+            dfs(node.left, result)
+            dfs(node.right, result)
+
+            return result
+        return dfs(root, result)
+
+    # Space Complexity = O(n)
+    # Time Complexity = O(n)
