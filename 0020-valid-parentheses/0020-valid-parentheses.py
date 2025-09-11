6c9a1c60aaeba9fb93fdce583f29999622f36678
Uzoma Stephanie-Portia Okey-Anyanwu <166033308+uzom-a@users.noreply.github.com>
2025-09-11 10:27:18 -0500

    Time: 4 ms (10.01%), Space: 18.1 MB (11.85%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        brackets = {"(": ")", "[":"]", "{":"}"}
+        stack = []
+
+        for ch in s:
+            if ch in brackets:
+                stack.append(ch)
+                continue
+            # print(stack)
+
+            if stack and brackets[stack[-1]] == ch:
+                stack.pop()
+            else:
+                return False
+
+
+        return not stack
