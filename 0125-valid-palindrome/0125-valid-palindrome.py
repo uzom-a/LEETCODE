725eacdf6c63a113356695b11bbeb5e5e3dc03db
Uzoma Stephanie-Portia Okey-Anyanwu <166033308+uzom-a@users.noreply.github.com>
2025-09-22 00:35:29 -0500

    Time: 8 ms (53.1%), Space: 17.8 MB (99.34%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,19 +1,21 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.strip().lower().replace(" ", "")
-        s = "".join(ch for ch in s if ch.isalnum())
-        # print(s)
-        
         i = 0
         j = len(s) - 1
 
-        while i <= j:
-            if s[i] != s[j]:
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+
+            while i < j and not s[j].isalnum():
+                j -= 1
+
+            if s[i].lower() != s[j].lower():
                 return False
+
             i += 1
             j -= 1
 
         return True
-
-        #Time Complexity = O(n)
-        #Space Complexity = O(n)
+        #O(1) Space complexity
+        #O(n) Time Complexity
