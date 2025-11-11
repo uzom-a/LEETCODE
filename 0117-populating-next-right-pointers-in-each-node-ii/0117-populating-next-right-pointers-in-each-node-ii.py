5af3e111e536baa4b5669f50d1abe9653cf98f37
Uzoma Stephanie-Portia Okey-Anyanwu <166033308+uzom-a@users.noreply.github.com>
2025-11-10 19:29:08 -0600

    Time: 45 ms (62.18%), Space: 18.7 MB (82.97%) - LeetHub

---
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
@@ -0,0 +1,42 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        """
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+from collections import deque
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return root
+        q = deque([root])
+        
+
+        while q:
+            size = len(q) #size is dynamic so you can't just intialize it outside unless it would always be 0
+            for i in range(size):
+                node = q.popleft()
+
+                if i < size - 1:
+                    node.next = q[0]
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+        return root
