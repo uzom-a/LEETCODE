b9b3d4d2484c769e3403ee77d1bd180f08baa04c
Uzoma Stephanie-Portia Okey-Anyanwu <166033308+uzom-a@users.noreply.github.com>
2026-09-12 17:40:54 -0500

    Time: 0 ms (100%), Space: 19.4 MB (10.94%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -6,13 +6,11 @@ class Solution:
                 stack.append(s[i])
             else:
                 substr = ""
-                while stack[-1] != "[":
+                while stack and stack[-1] != "[":
                     substr = stack.pop() + substr
                 stack.pop()
                 k = ""
                 while stack and stack[-1].isdigit():
                     k = stack.pop() + k
-                
-                stack.append(int(k) * substr)
-
+                stack.append(int(k)* substr)
         return "".join(stack)
